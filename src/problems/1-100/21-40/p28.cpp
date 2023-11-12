54aed49ab8153ae6d9ab6f6a119cf9c72751ea49
AwesomeCodingGuy <sonla@gmx.net>
2023-11-12 16:13:22 +0100

    Added solution for problem 28.

---
+++ src/problems/1-100/21-40/p28.cpp
@@ -0,0 +1,17 @@
+#include "problems/problem_registry.hpp"
+
+int64_t problem_28()
+{
+    int64_t sum = 1;
+
+    for(int64_t spiralSize = 3; spiralSize <= 1001; spiralSize+=2) {
+        int64_t square = spiralSize * spiralSize;
+        for(int64_t i = 0; i < 4; ++i) {
+            sum += square - (spiralSize - 1) * i;
+        }
+    }
+
+    return sum;
+}
+
+REGISTER_FUNCTION(28, problem_28);
